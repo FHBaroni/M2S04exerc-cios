0c3bcbaa1e09821969922fb6494c2af1353b2a80
Jamil Salim Daruix <jamil.daruix@pixeon.com>
2022-06-23 21:47:51 -0300

    Desenho da tela

---
+++ ExercicioMod2-Semana4/ExercicioMod2-Semana4/Form1.cs
@@ -0,0 +1,33 @@
+namespace ExercicioMod2_Semana4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Aqui será o momento de simular a inscrição do aluno
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSalvarDados_Click(object sender, EventArgs e)
+        {
+            /*ToDo: Códigos*/
+
+            MessageBox.Show("Olá Mundo !!!");
+            MessageBox.Show($"Nome {txtNome.Text}");
+            txtIdade.Text = "37";
+        }
+
+        private void btnLimparCampos_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            txtCurso.Text = "";
+            txtEscolaridade.Text = "";
+            txtDataNascimento.Text = "";
+            txtIdade.Text = "";
+        }
+    }
+}
